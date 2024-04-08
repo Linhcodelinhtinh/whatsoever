2cb8eae6e7a0ff0efcd681524598097653b7ef03
Lưu Quang Linh <149509378+Linhcodelinhtinh@users.noreply.github.com>
2024-04-08 14:55:59 +0700

    Add files via upload

---
+++ HW1/AboutMe.java
@@ -0,0 +1,11 @@
+package OOP.HW1;
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Họ và tên: Lưu Quang Linh.\tMSSV: 23020392 \tLớp: K68AI2 \tUsername: Linhcodelinhtinh\tEmail: 23020392@vnu.edu.vn");
+        for(int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.\n" +
+                    "Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
